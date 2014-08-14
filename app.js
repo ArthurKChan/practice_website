d2e9649ce4d74e0fe198238cc8d043f1974ccecc
Arfurboy <Arfurboy@gmail.com>
2014-08-13 17:41:27 -0700

    Removed g+ button, Testing jquery and button styles

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 $(document).ready(main);
 
 function main(){
-$('body').append("<p>This is my first paragraph of words</p>");
-$('body').append("<h4>UNORDERED LIST JUNK</h4>");
-$('h4').after("<ul class='list'></ul>");
-$('body').find('.list').append("<li>hello</li>");
-
 $('body').prepend('<button class="g-signin"></button>');
 
-
-
+var score = 0;
+$('body').on('click', '.trigger', function(){
+  score++;
+  $('body').find('#score').text(score);
+});  
+  
 }
